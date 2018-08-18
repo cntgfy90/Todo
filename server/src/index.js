5f7b89de2edfbadd6dd565ad63190ce50b9ecdfc
cntgfy90 <stepan.kolomiyets@gizzaro.io>
2018-08-19 00:51:08 +0300

    Initial commit with basic setup

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds121726.mlab.com:21726/todolist');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Server is up and running on ${port} port`);
+});
